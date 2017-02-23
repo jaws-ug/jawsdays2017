9df6792349524ad668faca84f209fdf8747b8282
Kazue IGARASHI <gatespace.k@gmail.com>
2017-02-23 16:03:12 +0900

    TinyMCEカスタマイズ

--- wp-content/themes/jawsdays/inc/extras.php
+++ wp-content/themes/jawsdays/inc/extras.php
@@ -70,3 +70,34 @@ jQuery(document).ready(function($){
 </script>
 <?php
 }
+
+/**
+ * TinyMCE
+ */
+// Callback function to insert 'styleselect' into the $buttons array
+function jawsdays_mce_buttons_2( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'jawsdays_mce_buttons_2' );
+
+// Callback function to filter the MCE settings
+function jawsdays_mce_before_init_insert_formats( $init_array ) {
+	// Define the style_formats array
+	$style_formats = array(
+		// Each array child is a format with it's own settings
+		array(  
+			'title' => 'A ボタン',
+			'inline' => 'a',
+			'classes' => 'btn'
+		),
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );
+
+	return $init_array;  
+
+}
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'jawsdays_mce_before_init_insert_formats' );
